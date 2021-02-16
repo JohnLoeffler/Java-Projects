c6ba69da033da88507c3c3888d648687e56d8fb1
John <John.Loeffler@gmail.com>
2021-02-16 09:06:22 -0500

    Main function now validates object creation and tests the Rational class's operation and output methods.

--- CISC7124X/Assignment_2/assignment_2/Assignment_2_Driver.java
+++ CISC7124X/Assignment_2/assignment_2/Assignment_2_Driver.java
@@ -70,7 +70,6 @@ public class Assignment_2_Driver{
       System.err.println(e.getMessage());
       e.printStackTrace(System.err);
     }
-
     /* Test the Subtraction method */
     try {
       Rational result = lhs.Subtract(rhs);
